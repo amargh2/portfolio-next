6bc8a3df7750b4565d4c5181bded3a94aaee0abb
anthony <margherioa@gmail.com>
2023-04-23 01:20:41 -0500

    working on the project cards

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,14 @@
 import Nav from "@/components/Nav"
+import projects from "@/components/projects"
+import ProjectCard from "@/components/ProjectCard"
 export default function Projects() {
+  
+  const projectCards = [projects.forEach(project => (<ProjectCard project={project}/>))]
+  console.log(projectCards)
     return (
         <>
             <Nav selected='projects'/>
             <div className='grid grid-cols-1 md:grid-cols-6'>
-                
             </div>
         </>
     )
