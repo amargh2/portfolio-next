d1dd20527723bb1f613e8fb5464f518e4b56dc70
anthony <margherioa@gmail.com>
2023-04-24 15:37:15 -0500

    added titles for the pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,36 @@ import Carousel from '@/components/Carousel'
 import HomeAboutPanel from '@/components/HomeAboutPanel'
 import SkillsPanel from '@/components/SkillsPanel'
 import Footer from '@/components/Footer'
+import Head from 'next/head'
 export default function Home() {
   return (
-    <div className='flex flex-col bg-gradient-to-br from-blue-100 to-white gap-2'>  
-      <Nav selected= 'home'/>
-      <div className='grid gap-3 grid-cols-1 md:grid-cols-10 px-2 md:px-20 mb-10'>
-      <div className='col-start-1 col-span-1 row-span-3 flex justify-center mt-10'>
-      </div>
+    <>  
+      <Head>
+        <title>Home - Anthony Margherio</title>
+        <meta
+          name="description"
+          content="I’m Anthony Margherio. I'm a full-stack developer in Chicago, Illinois."
+        />
+      </Head>
+      <div className='flex flex-col bg-gradient-to-br from-blue-100 to-white gap-2'>  
+        <Nav selected= 'home'/>
+        <div className='grid gap-3 grid-cols-1 md:grid-cols-10 px-2 md:px-20 mb-10'>
+        <div className='col-start-1 col-span-1 row-span-3 flex justify-center mt-10'>
+        </div>
 
-      <div className='md:col-start-2 md:col-span-8'>
-          <HomeAboutPanel/>
-        </div>
         <div className='md:col-start-2 md:col-span-8'>
-          <SkillsPanel/>
+            <HomeAboutPanel/>
+          </div>
+          <div className='md:col-start-2 md:col-span-8'>
+            <SkillsPanel/>
+          </div>
+
+          <div className='md:col-start-2 md:col-span-8'>  
+              <Carousel />
+          </div>
         </div>
-        
-        <div className='md:col-start-2 md:col-span-8'>  
-            <Carousel />
-        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
     )
 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,27 +1,37 @@
 import Footer from "@/components/Footer"
 import Nav from "@/components/Nav"
 import ProjectGrid from "@/components/ProjectGrid"
+import Head from "next/head"
 export default function ProjectsPage() {
-    // Project Grid creates Project Cards and places them in a Grid
-    return (
-        <div className='flex flex-col mb-3'>
-            <Nav selected='projects'/>
-            <div className= 'grid grid-cols-1 px-10 md:grid-cols-3 md:px-32 gap-10 mt-4 md:mt-4 mb-10'>
-            <div className="mt-6 space-y-7 text-base text-zinc-600 col-start-1 col-span-3 lg:mx-40">
-                <h1 className="text-4xl flex justify-center font-bold tracking-tight text-center text-zinc-800 sm:text-5xl">
-                    Some of my projects so far
-                </h1>
-                  <p>
-                    This is a selection of some projects I have built so far.
-                    Most of these are assignments from The Odin Project, an open source community and course that challenges
-                    learners to build projects from scratch after learning and reading about concepts.
-                    Building these projects has been a lot of fun, and I am currently working on a couple of
-                    app ideas of my own.
-                  </p>
-            </div>
-            </div>
-            <ProjectGrid/>
-            <Footer/>
+  // Project Grid creates Project Cards and places them in a Grid
+  return (
+    <>
+      <Head>
+        <title>Projects - Anthony Margherio</title>
+        <meta
+          name="description"
+          content="Anthony Margherio's projects."
+      />
+      </Head>  
+      <div className='flex flex-col mb-3'>
+        <Nav selected='projects'/>
+        <div className= 'grid grid-cols-1 px-10 md:grid-cols-3 md:px-32 gap-10 mt-4 md:mt-4 mb-10'>
+        <div className="mt-6 space-y-7 text-base text-zinc-600 col-start-1 col-span-3 lg:mx-40">
+            <h1 className="text-4xl flex justify-center font-bold tracking-tight text-center text-zinc-800 sm:text-5xl">
+                Some of my projects so far
+            </h1>
+              <p>
+                This is a selection of some projects I have built so far.
+                Most of these are assignments from The Odin Project, an open source community and course that challenges
+                learners to build projects from scratch after learning and reading about concepts.
+                Building these projects has been a lot of fun, and I am currently working on a couple of
+                app ideas of my own.
+              </p>
         </div>
-    )
+        </div>
+        <ProjectGrid/>
+        <Footer/>
+      </div>
+    </>
+  )
 }
