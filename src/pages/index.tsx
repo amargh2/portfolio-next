226316d5de25b621537d6d0590d45401ad974080
anthony <margherioa@gmail.com>
2023-04-24 12:18:42 -0500

    added gap to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import SkillsPanel from '@/components/SkillsPanel'
 import Footer from '@/components/Footer'
 export default function Home() {
   return (
-    <div className='flex flex-col bg-gradient-to-br from-blue-100 to-white'>  
+    <div className='flex flex-col bg-gradient-to-br from-blue-100 to-white gap-2'>  
       <Nav selected= 'home'/>
       <div className='grid gap-3 grid-cols-1 md:grid-cols-10 px-2 md:px-20 mb-10'>
       <div className='col-start-1 col-span-1 row-span-3 flex justify-center mt-10'>
