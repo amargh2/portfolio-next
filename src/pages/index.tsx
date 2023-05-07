436ae61d3bdafc621f7210a24dc65fc20568b5b8
anthony <margherioa@gmail.com>
2023-05-07 16:48:18 -0500

    changes to margins and spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,8 @@ export default function Home() {
       </Head>
       <div className='bg-blue-100'><Nav selected= 'home'/></div>
       <div className='flex flex-col bg-gradient-to-b from-blue-100 to-white gap-2'>  
-        <div className='grid gap-3 grid-cols-1 md:grid-cols-10 px-2 md:px-20 mb-10'>
-        <div className='col-start-1 col-span-1 row-span-3 flex justify-center mt-10'>
+        <div className='grid gap-3 grid-cols-1 md:grid-cols-10 px-2 md:px-20 md:mt-5 mb-10'>
+        <div className='col-start-1 col-span-1 row-span-3 flex justify-center'>
         </div>
 
         <div className='md:col-start-2 md:col-span-8'>
