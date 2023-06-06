d2c8f52d3af490068ee2e7831bc98ebb50e808a6
Anthony <margherioa@gmail.com>
2023-06-06 15:32:37 -0500

    text changes, picture update, got rid of background color on main page

--- src/components/HomeAboutPanel.tsx
+++ src/components/HomeAboutPanel.tsx
@@ -6,8 +6,8 @@ export default function HomeAboutPanel() {
     return (    
     <div className='flex md:flex-row flex-col bg-white rounded-lg gap-2 shadow-md p-2 ring-1 ring-indigo-200'>
           <div className='flex flex-col items-center'>
-            <Image src='/me.jpg' alt='Photo of Anthony.' width={500} height={100} className='rounded-full hidden md:flex'/>
-            <Image src='/me.jpg' alt='Photo of Anthony.' width={200} height={100} className='rounded-full md:hidden'/>
+            <Image src='/me3.jpg' alt='Photo of Anthony.' width={500} height={100} className='rounded-full hidden md:flex'/>
+            <Image src='/me3.jpg' alt='Photo of Anthony.' width={200} height={100} className='rounded-full md:hidden'/>
           <div className='text-slate-90 mt-2 flex justify-center p-2'>
             <Link href='https://www.github.com/amargh2'>    
               <BsGithub size={30}/>
@@ -15,7 +15,7 @@ export default function HomeAboutPanel() {
           </div>  
         </div>
         <div>  
-          <div className='flex justify-center font-bold text-lg text-center'> Hi there, I'm Anthony. I'm a self-taught developer in Chicago.</div>
+          <div className='flex justify-center font-bold text-lg text-center'> Hi there, I'm Anthony.</div>
           <div className='flex justify-center m-10'>
             I'm a curious lifelong learner with a love for tech, travel, 
             reading, and writing.
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -27,8 +27,8 @@ export default function About () {
           </div>
           <div className="lg:order-second lg:row-span-2 flex justify-center m-2">
             <div className="space-y-7 text-base text-zinc-600">
-            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              Hi, I'm Anthony Margherio. I'm a self-taught developer living in Chicago, Illinois.
+            <h1 className="text-lg font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+              Hi, I'm Anthony. I'm a tech and travel enthusiast living in Chicago.
             </h1>
               <p>
                 I grew up in a small town in Illinois dreaming of seeing the world,
@@ -45,7 +45,8 @@ export default function About () {
                 After earning some certifications in networking and operating systems 
                 through self-study, I decided to
                 start learning full-stack development through a free, open source bootcamp and 
-                learning community called The Odin Project. Learning to build functioning apps out of code 
+                learning community called The Odin Project. Learning to code tied lots of concepts together
+                for me and 
                 felt like a whole new adventure, so
                 after a lot of soul searching I decided to leave flying in the spring of 2022 to 
                 study full-time, get my feet on the ground, and 
@@ -59,13 +60,13 @@ export default function About () {
                 TypeScript. I'm hoping to continue to learn and contribute my existing skills
                 to a great team. While I'm open to many opportunities, I am especially interested 
                 in front and back-end development, infrastructure, and computer networking. I
-                also have a BA in English and exposure to markdown - I'll always be happy to contribute
+                also have a BA in English and know markdown - I'll always be happy to contribute
                 to the team's documentation efforts!
               </p>
               <p> 
                 When I'm not coding or studying, I like spending time running mid to long
                 distances, playing the occasional video game, listening to podcasts, or reading fantasy or
-                nonfiction. I also meditate daily, am a vegetarian, and consider myself a pretty calm, kind guy who 
+                nonfiction. I also meditate daily, am a vegetarian, and consider myself a pretty calm, kind person who 
                 tries to find the fun in life. 
                 Thanks for passing through, and don't hesitate to reach out if you'd like to work together. :)
                  
